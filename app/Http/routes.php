804ec111978cdc535a60b57a64f7c83885c4f59d
Sumit Yadav <ysumit99@gmail.com>
2017-07-11 01:41:34 +0530

    Comments basic layout

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -31,6 +31,9 @@
 	Route::resource('categories', 'CategoryController', ['except' => ['create']]);
 	Route::resource('tags', 'TagController', ['except' => ['create']]);
 
+	//comments
+	Route::post('comments/{post_id}', ['uses' => 'CommentsController@store', 'as' => 'comments.store']);
+
 
 	Route::get('blog/{slug}', ['as' => 'blog.single', 'uses' => 'BlogController@getSingle'])->where('slug', '[\w\d\-\_]+');
 	Route::get('blog', ['uses' => 'BlogController@getIndex', 'as' => 'blog.index']);
--- app/Post.php
+++ app/Post.php
@@ -15,4 +15,10 @@ class Post extends Model
     {
     	return $this->belongsToMany('App\Tag');
     }
+
+    public function comments()
+    {
+    	return $this->hasMany('App\Comment');
+    }
+    
 }
